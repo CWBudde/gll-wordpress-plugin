8c311de14740ea852f26ef24713cc394584f248f
Christian Budde <4097562+CWBudde@users.noreply.github.com>
2026-01-31 15:13:10 +0100

    chore: more refinement and work

--- build/gll-info/index.asset.php
+++ build/gll-info/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '99f4689b1cb0ea26d745');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '961b9de3674e895202c4');
