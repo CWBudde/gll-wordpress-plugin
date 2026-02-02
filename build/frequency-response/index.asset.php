8fe2eb8404f55cb48ecb5dbc64948a9cbc7ab92b
Christian-W. Budde <Christian@pcjv.de>
2026-02-02 06:17:03 +0100

    chore: more fixes

--- build/frequency-response/index.asset.php
+++ build/frequency-response/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e4a064a85c87472cee6b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cee6335f41f542c68fd5');
--- build/gll-info/index.asset.php
+++ build/gll-info/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '179fd3b5f5a713158663');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2a21e41b6af86c0aafa9');
